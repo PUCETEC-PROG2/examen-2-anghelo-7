693e96c925434d7c8edd4a98ea42f507fac1d78b
Anghelo <Anghelo>
2024-07-13 10:31:48 -0500

    Modificado

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=30, null=False)
+    genre = models.CharField(max_length=30, null=False)
+    direction = models.CharField(max_length=30, null=False)
+    year_publication = models.IntegerField(null = False)
+    synopsis = models.TextField(max_length=200, null=False)
+    
+    def __str__(self) -> str:
+        return self.title
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Movie
 
 # Create your views here.
+
+
+
+def index(request):
+    films = Movie.objects.order_by('title')
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'films': films}, request))
+
+def movies(request, movie_id):
+    movie = movie.objects.get(pk = movie_id)
+    template = loader.get_template('display_movies.html')
+    context = {
+        'movie': movie
+    }
+    return HttpResponse(template.render(context, request))
